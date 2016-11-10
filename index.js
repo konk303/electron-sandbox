a583d1f0f5df10cb0b4e0187d8b052e47b25859d
Kimihiko NAKAMURA <nakamura.808@gmail.com>
2016-11-10 15:46:03 +0900

    playing with multiple window, file download

--- index.js
+++ index.js
@@ -1,55 +1,54 @@
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
-const url = require('url')
+console.log('hoi');
+let lastSavePath = window.localStorage.getItem('lastSavePath') || '';
+let filename = 'foo.txt';
+let main = require('electron').remote;
+console.log(main);
+main.dialog.showSaveDialog({
+  defaultPath: lastSavePath + filename,
+  title: 'Download File',
+  filters: [{
+    name: "File",
+    extensions: [filename.split('.').pop()]
+  }]
+}, (filePath) => {
+  console.log(filePath);
+  let fs = require('fs');
+  let savedFileName = filePath.split('/').pop();
+  window.localStorage.setItem('lastSavePath', filePath.replace(savedFileName, ''));
+  fs.writeFile(filePath, 'foobar', 'binary', (err) => {});
+});
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let win
+// let request = require('request');
 
-function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+// // ①
+// request(url, {
+//   encoding: 'binary'
+// }, (error, response, body) => {
+//   if (!error) {
+//     let remote = require('remote');
 
-  // and load the index.html of the app.
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+//     // ②
+//     let lastSavePath = window.localStorage.getItem('lastSavePath') || '';
 
-  // Open the DevTools.
-  win.webContents.openDevTools()
+//     // ③
+//     remote.require('dialog').showSaveDialog({
+//       defaultPath: lastSavePath + filename,
+//       title: 'Download File',
+//       filters: [{
+//         name: "File",
+//         extensions: [filename.split('.').pop()]
+//       }]
+//     }, (filePath) => {
 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null
-  })
-}
+//       // ④
+//       if (filePath) {
+//         let fs = require('fs');
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
-
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
-  }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+//         // ⑤
+//         let savedFileName = filePath.split('/').pop();
+//         window.localStorage.setItem('lastSavePath', filePath.replace(savedFileName, ''));
+//         fs.writeFile(filePath, body, 'binary', (err) => {});
+//       }
+//     });
+//   }
+// });
